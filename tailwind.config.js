5b0f353cafa1f79be61536b89fc022d17b292756
MadeAditya02 <imadeaditya4@gmail.com>
2022-03-18 15:47:32 +0800

    Use local tailwind (JIT) instead use cdn

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+		container: {
+			center: true,
+		},
+		fontFamily: {
+			readex: ['Readex Pro', 'sans-serif'],
+		}
+	},
+  plugins: [],
+}
